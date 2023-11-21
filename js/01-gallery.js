3fea9fb62848d6ac4ec47704fbf7dea91c6f42b6
patryq03 <patryk.kuca@o2.pl>
2023-11-21 20:49:49 +0100

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const div = document.querySelector("ul.gallery");
+for (const image of galleryItems) {
+    const html = `<div class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img
+        class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+      />
+    </a>
+  </div>`;
+  div.insertAdjacentHTML('beforeend', html);
+}
 console.log(galleryItems);
