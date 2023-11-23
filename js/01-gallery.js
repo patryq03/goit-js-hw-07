c89f7b8f9b61a0f10d87c9ee619268e0bc0b3abf
patryq03 <patryk.kuca@o2.pl>
2023-11-23 21:34:58 +0100

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const div = document.querySelector("ul.gallery");
-div.addEventListener("click", (event) => {
-  event.preventDefault();
-  const lightbox = basicLightbox.create(div);
-  lightbox.show();
-});
+let instance;
 for (const image of galleryItems) {
   const html = `<div class="gallery__item">
     <a class="gallery__link" href=${image.original}>
@@ -19,3 +15,27 @@ for (const image of galleryItems) {
   </div>`;
   div.insertAdjacentHTML("beforeend", html);
 }
+
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    const selectedImage = event.target.dataset.source;
+    instance = basicLightbox.create(
+      `
+  		<img width="1400" height="900" src="${selectedImage}">
+  	`
+    );
+    instance.show();
+
+    document.addEventListener("keydown", closePreview);
+  }
+}
+
+function closePreview(event) {
+  if (event.key == "Escape") {
+    console.log("naciśnięto esc");
+    document.removeEventListener("keydown", closePreview);
+    instance.close();
+  }
+}
+div.addEventListener("click", handleClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,16 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const div = document.querySelector("ul.gallery");
-div.addEventListener("click", (event) => {
-    event.preventDefault();
-var lightbox = new SimpleLightbox(' a.gallery__item');
-lightbox.show();
-});
+
 for (const item of galleryItems) {
   const html = `<a class="gallery__item" href="${item.original}">
     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
   </a>`;
-  div.insertAdjacentHTML('beforeend', html);
-
-
+  div.insertAdjacentHTML("beforeend", html);
 }
-console.log(galleryItems);
-const altTimer = document.querySelector(`img[alt=${image.description}]`);
-altTimer.addEventListener('click', _.debounce( () =>{
- altTimer.show();
-},250));
-console.log(html);
+const galleryImages = document.querySelectorAll(".gallery__item");
+new SimpleLightbox(galleryImages, {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
